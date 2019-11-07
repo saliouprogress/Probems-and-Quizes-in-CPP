6ccc2b79e5eadd489d9de3210c2794ea6a10d19d
Mamadou Diallo <49839551+saliouprogress@users.noreply.github.com>
2019-11-07 13:58:36 -0500

    Create CountrepeatCharInWord.cpp

---
+++ CountrepeatCharInWord.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+void print(const std::string& infix, const std::string& comp)
+{
+  int count = 0;
+  for(auto c : infix)
+  {
+    if (c == 'm') {count++; std::cout << (int)c << std::endl;}
+  }
+  cout <<  "count: " << count << std::endl;
+}
+
+int main()
+
+{
+  string strf = "mamadou";
+  string strl = "saliou";
+  //int a = strf.length();
+  //cout << a << endl;
+  print(strf,strl);
+  int i = 0; 
+  while (str[i] != '\0') 
+  { 
+    ++i; 
+  } 
+  cout << i << endl; 
+  
+    // 5. size of string object using for loop 
+  for (i=0; str[i]!='\0'; i++) 
+  { 
+  } 
+  cout << i << endl; 
+  
+  std::cout << "Printable ASCII:\n";
+  for (char i = 32; i < 127; ++i) 
+  {
+    std::cout << i << ' ';
+    if (i % 16 == 15)
+      std::cout << '\n';
+  }
+ int b = ' ', c = ' ';
+ cout << b * c << endl;
+ 
+ int var=65;
+ cout<<(char)var<<endl;
+ 
+ return 0;
+}
