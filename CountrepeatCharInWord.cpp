d9b7da9f39f026941480ca01577a3f9faa108a36
Mamadou Diallo <49839551+saliouprogress@users.noreply.github.com>
2019-11-07 14:54:48 -0500

    Update CountrepeatCharInWord.cpp

--- CountrepeatCharInWord.cpp
+++ CountrepeatCharInWord.cpp
@@ -8,9 +8,9 @@ using namespace std;
 void print(const std::string& infix, const std::string& comp)
 {
   int count = 0;
-  for(auto c : infix)
+  for (auto c = infix.begin(); c!=infix.end(); ++c)
   {
-    if (c == 'm') {count++; std::cout << (int)c << std::endl;}
+    if (*c == 'm') {count++; std::cout << (int)(*c) << std::endl;}
   }
   cout <<  "count: " << count << std::endl;
 }
@@ -23,15 +23,20 @@ int main()
   //int a = strf.length();
   //cout << a << endl;
   print(strf,strl);
-  int i = 0; 
-  while (str[i] != '\0') 
+  
+ return 0;
+}
+
+/*
+int i = 0; 
+  while (strf[i] != '\0') 
   { 
     ++i; 
   } 
   cout << i << endl; 
   
     // 5. size of string object using for loop 
-  for (i=0; str[i]!='\0'; i++) 
+  for (i=0; strf[i]!='\0'; i++) 
   { 
   } 
   cout << i << endl; 
@@ -49,5 +54,4 @@ int main()
  int var=65;
  cout<<(char)var<<endl;
  
- return 0;
-}
+*/
