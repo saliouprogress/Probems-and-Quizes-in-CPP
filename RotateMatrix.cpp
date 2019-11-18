329c4683d5a42ad2d47f9379e2f28ed0ec00a224
Mamadou Diallo <49839551+saliouprogress@users.noreply.github.com>
2019-11-18 18:32:02 -0500

    Update RotateMatrix.cpp

--- RotateMatrix.cpp
+++ RotateMatrix.cpp
@@ -11,7 +11,7 @@ vector<string> split(const string &);
 void matrixRotation(vector<vector<int>> matrix, int r) 
 {
   int raw = matrix.size(), column = (matrix[0]).size(), moveAlongRaw = 0, moveAlongColumn = 0;
-  vector<vector<int>> rotated; int indexI = 1, indexJ = 1; 
+  vector<vector<int>> rotated; int indexI = 0, indexJ = 0; 
   
   cout << "before rotation " << matrix[indexI][indexJ] << endl;
   for (int i = 0; i < matrix.size(); i++)
@@ -42,12 +42,23 @@ void matrixRotation(vector<vector<int>> matrix, int r)
   else
   { 
     r += -moveAlongColumn;
-    //indexJ += moveAlongColumn;
+    indexJ += moveAlongColumn;
   }
   
   cout << "r " << r;
   
   cout << "after rotation " << matrix[indexI][indexJ] << endl;
+  
+  cout << "raw: " << moveAlongRaw << ", Column " << moveAlongColumn << endl;
+  
+  int Corner1I, Corner1J, Corner2I, Corner2J, Corner3I, Corner3J, Corner4I, Corner4J;
+  
+  if ( 2*indexI < raw) {Corner1I = indexI; cout << Corner1I << endl;}
+  else { cout << "Bottom" << endl;}
+  if ( 2*indexJ < column) {Corner1J = indexJ; cout << Corner1J << endl;}
+  else { cout << "Right" << endl;}
+  
+  
   
 }
 
