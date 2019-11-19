442ab67a33cc4570644985faf2d362c88bd336e2
Mamadou Diallo <49839551+saliouprogress@users.noreply.github.com>
2019-11-19 15:04:49 -0500

    Update LengthOfSubarray.cpp

--- LengthOfSubarray.cpp
+++ LengthOfSubarray.cpp
@@ -1,21 +1,26 @@
-#include <iostream>
+#include <iostream>      // Find how many times the most repeated number appears
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
 int FindLength(vector<int>& arr)
-{ int freq = 0; unordered_map<int, int> storefreg;
+{ 
+  int freq = 1; unordered_map<int, int> storefreq;
 
-  for(int index = 0; index < arr.size(); i++)
+  for(int index = 0; index < arr.size(); index++)
   {
-    storefreq[arr[i]]++;
+    storefreq[arr[index]]++;
+    freq = max(freq, storefreq[arr[index]]);
+    cout << index << " " << storefreq[arr[index]] << endl;
   }
-
+  return freq;
 }
 
 int main()
 {
-  
+  vector<int> mar = {1,3,4,6,2,2,5,0};
+  cout << " max " << FindLength(mar) << endl;
 
   return 0;
 }
