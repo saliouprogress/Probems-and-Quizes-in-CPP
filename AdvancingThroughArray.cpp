dd4cf07b967f8b3a428671a710ce2830ac11f4b2
Mamadou Diallo <49839551+saliouprogress@users.noreply.github.com>
2019-11-19 12:00:01 -0500

    Update AdvancingThroughArray.cpp

--- AdvancingThroughArray.cpp
+++ AdvancingThroughArray.cpp
@@ -9,18 +9,18 @@ bool CanReachEnd(const vector<int>& max_advance_steps)
   int last_index = max_advance_steps.size() -1; // get last index
   for (int i = 0; i <= furthest_reach_so_far && furthest_reach_so_far < last_index; i++) // iterate from first to last index
   { 
-    cout << "furthest_reach_so_far " << furthest_reach_so_far << endl; // output current index
-    furthest_reach_so_far = max(furthest_reach_so_far, max_advance_steps[i] +i);
+    cout << max_advance_steps[i] << " furthest_reach_so_far " << furthest_reach_so_far << endl; // output current index
+    furthest_reach_so_far = max(furthest_reach_so_far, max_advance_steps[i] +i); // 
     //cout << "furthest_reach_so_far " << furthest_reach_so_far << endl;
   }
-  cout << "furthest_reach_so_far " << furthest_reach_so_far << endl;
-  return furthest_reach_so_far >= last_index;
+  cout << "furthest_reach_so_far " << furthest_reach_so_far << endl; // print last index reached
+  return furthest_reach_so_far >= last_index; // true if last index reahed is bigger or equal to last index in array
  }
 
 int main()
 {
-  vector<int> mar = {3,3,1,0,2,0,1};
-  cout << CanReachEnd(mar) << endl;
+  vector<int> mar = {3,3,1,0,2,0,1}; // input data
+  cout << CanReachEnd(mar) << endl; // 
 
   return 0;
 }
