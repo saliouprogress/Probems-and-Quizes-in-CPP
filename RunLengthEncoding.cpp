2c03311fc2d8c3bccbb3aed7b6ddb456db792a5f
Mamadou Diallo <49839551+saliouprogress@users.noreply.github.com>
2019-11-23 12:13:19 -0500

    Create RunLengthEncoding.cpp

---
+++ RunLengthEncoding.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string CountElement(const string& mar)
+(
+  unordered_map<char, int> store;
+  for (int i = 0; i < mar.size(); i++)
+  {
+    store[mar[i]]++;
+  }
+  
+)
+
+int main()
+{
+  
+
+
+  return 0;
+}
