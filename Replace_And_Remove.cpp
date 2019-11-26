084eb4e24c2e3429d53fef6e4985cd03d8a5cf54
Mamadou Diallo <49839551+saliouprogress@users.noreply.github.com>
2019-11-25 21:45:57 -0500

    Update Replace_And_Remove.cpp

--- Replace_And_Remove.cpp
+++ Replace_And_Remove.cpp
@@ -5,31 +5,44 @@
 
 using namespace std;
 
+string RemoveElement( int i, int remove, string num)
+{
+   int current = 0;
+  while (num[i] != NULL) 
+  {
+     if (num[i] == remove) 
+     {num[i] = num[i + ++current];}
+     else { num[i] = num[i + current];}
+     i++; 
+  }
+  //for (int x = i; x >=0; --x) { cout <<  num[x] <<  " " << x << endl;}
+  return num;
+}
 
-char DecimalToOtherBase(string num, char replace, char remove, char substitute)
+int DecimalToOtherBase(string num, char replace, char remove, char substitute)
 { 
   string tempVec[32];
-  int i = 0, countReplace = 0, countRemove = 0, current = 0, next = current + 1;
-  while (num[i] != '\0') 
-  { 
+  int i = 0, countReplace = 0, current = 0, next = current + 1;
+  while (num[i] != NULL) 
+  {
+    if (num[i] == remove) 
+    { 
+      num = RemoveElement( i, remove, num); ++current;
+      //cout << num[i] <<  " " << i << endl;
+    }
+    else{ num[i] = num[ i + current];}
+    i++; 
     
-    if (num[i] == replace) { countReplace += 2; }
-    if (num[i] == remove) { num[i] = num[i + ++countRemove]; i = i-1;}
-    else { num[i] = num[i + countRemove];}
-    tempVec[i] = num[i];
-    i++;             
   }
-  tempVec[i] = num[i];
-  for (int x = i-1; x >=0; --x) { cout << tempVec[x] <<  " " <<  x <<  endl;}
-  cout << countRemove << endl;
-  return replace;
+  //tempVec[i] = num[i];
+  for (int x = i; x >=0; --x) { cout <<  num[x] <<  " " << x << endl;}
+  return current;
 }
 
 
 int main()
 {
-  char replace = 'a', remove = 'b' , substitute = 'd'; string mystring = "bacdbbca";
+  char replace = 'a', remove = 'b' , substitute = 'd'; string mystring = "ahbcbdd";
   cout << DecimalToOtherBase(mystring, replace, remove, substitute) << endl;
-  
   return 0;
 }
