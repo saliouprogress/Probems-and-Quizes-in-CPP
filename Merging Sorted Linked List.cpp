2a6e395e7efa61eea0148d12897eb8f9af32346e
Mamadou Diallo <49839551+saliouprogress@users.noreply.github.com>
2019-12-04 22:10:27 -0500

    Update Merging Sorted Linked List.cpp

--- Merging Sorted Linked List.cpp
+++ Merging Sorted Linked List.cpp
@@ -18,6 +18,22 @@ void AppendNode(shared_ptr<ListNode<int>> *node, shared_ptr<ListNode<int>> *tail
   *node = (*node) -> next;
 }
 
+void push(shared_ptr<ListNode<int>> head_ref, int new_data) 
+{ 
+	/* allocate node */
+	shared_ptr<ListNode<int>> new_node(new ListNode<int>); 
+
+	/* put in the data */
+	new_node->data = new_data; 
+
+	/* link the old list off the new node */
+	new_node->next = head_ref; 
+
+	/* move the head to point to the new node */
+	head_ref = new_node; //cout << head_ref -> data << " ";
+} 
+
+
 shared_ptr<ListNode<int>> MergetwoLists( shared_ptr<ListNode<int>> L1, shared_ptr<ListNode<int>> L2)
 {
   shared_ptr<ListNode<int>> dummy_head(new ListNode<int>);
@@ -31,11 +47,38 @@ shared_ptr<ListNode<int>> MergetwoLists( shared_ptr<ListNode<int>> L1, shared_pt
   return dummy_head -> next;
 }
 
-
+void display(shared_ptr<ListNode<int>> head)
+  {
+    shared_ptr<ListNode<int>> temp(new ListNode<int>);
+    temp=head;
+    while(temp!=NULL)
+    {
+      cout<<temp->data<<"\t";
+      temp=temp->next;
+    }
+  } 
 
 int main()
 {
- 
+  shared_ptr<ListNode<int>> res = NULL; 
+  shared_ptr<ListNode<int>> a = NULL; 
+  shared_ptr<ListNode<int>> b = NULL; 
+  
+  
+  push(a, 15); 
+  push(a, 10); 
+  push(a, 5); 
+  push(a, 1); 
+
+  push(b, 20); 
+  push(b, 3); 
+  push(b, 2); 
+  
+  res = MergetwoLists(a, b); 
+
+  cout << "Merged Linked List is: \n"; 
+  display(res);
+	//printList(res); 
 
   return 0;
 }
